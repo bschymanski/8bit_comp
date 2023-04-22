cd1060add13a03d348f48a080cf7943e4c434aba
Garak <34037236+bschymanski@users.noreply.github.com>
2023-04-22 19:52:31 +0200

    assembler.py

--- software/microcode/left_rom.py
+++ software/microcode/left_rom.py
@@ -1,75 +1,114 @@
+import array
+import binascii
+#print (len(program))
+
 ea = [0xea];
 null = [0x00];
 
-pos_00 = 0x00
-pos_01 = 0x01
-pos_02 = 0x02
-pos_03 = 0x03
-pos_04 = 0x04
-pos_05 = 0x05
-pos_06 = 0x06
-pos_07 = 0x07
-pos_08 = 0x08
-pos_09 = 0x09
-pos_10 = 0x0a
-pos_11 = 0x0b
-pos_12 = 0x0c
-pos_13 = 0x0d
-pos_14 = 0x0e
-pos_15 = 0x0f
-pos_16 = 0x00
-pos_17 = 0x10
-pos_18 = 0x20
-pos_19 = 0x30
-pos_20 = 0x40
-pos_21 = 0x50
-pos_22 = 0x60
-pos_23 = 0x70
-pos_24 = 0x80
-pos_25 = 0x90
-pos_26 = 0xa0
-pos_27 = 0xb0
-pos_28 = 0xc0
-pos_29 = 0xd0
-pos_30 = 0xe0
-pos_31 = 0xf0
+# Instruction architecure
+# 
+# ........ ........ -- 
+# ........ ......00 -- stepping mode
+# ........ ......01 -- relative jump
+# ........ ......10 -- absolute jump
+# ........ ....0... -- 2nd operand is register
+# ........ ....1... -- 2nd operand is immediate
+# ........ dddd.... -- reg operand 1 ("RD")
+# ....ssss ........ -- reg operand 2 ("RS")
+# iiiiiiii ........ -- immediate
 
+
+# instructions
+# 00000000 00000000  - nop
+nop = 0b00000000
+#  
+ldi_r0 = 0b00011000
+ldi_r1 = 0b00101000
+#
+# move from rs to r0 or r1
+mv_r0 = 0b00010000
+mv_r1 = 0b00100000
+
+# iiiiiiii  
+# 00000001  - jrel +i Op2 is immediate
+jrel = 0b00001001
+#
+
+# jump relative imm
+jreli = 0b00001001
+#
+# jump relative rs
+jrelr = 0b00000010
+#
+# jump absolute imm
+jabsi = 0b00001001
+#
+# jump absolute rs
+jabsr = 0b00000010
+
+
+# aaaaaaaa
+# 00000010  - jabs a Op2 is immediate
+jabs = 0b00001010
+#
+# 00000000
+# 00000001  - halt
+halt = 0b00001001
+#
+# 00000000
+# 00000010  - reset
+reset = 0b00001010
+
+# register
+rd0 = 0b00010000
+rd1 = 0b00100000
+rs0 = 0b00000001
+rs1 = 0b00000010
+
+
+# here the array with the program.
+program = array.array('h', [
+    # Code Address
+    # Address 0 
+    nop, nop,
+    
+    # Address 1
+    ldi_r0, 0x01,
+    
+    # Address 2
+    ldi_r1, 0x02,
+
+    # Address 3
+    mv_r0, rs1,
+
+    # Address 4
+    ldi_r1, 0x04,
+    
+    # Address 5
+    mv_r1, rs0,
+
+    # Address 6
+    nop, 0x06,
+
+    # Address 7
+    nop, 0x07,
+
+    # Address 8
+    jabs, 0x09,
+
+    # 9
+    jrel, 0xf8
+])
 
 rom = bytearray(null * 32768)
-
-rom[0]  = pos_00
-rom[1]  = pos_01
-rom[2]  = pos_02
-rom[3]  = pos_03
-rom[4]  = pos_04
-rom[5]  = pos_05
-rom[6]  = pos_06
-rom[7]  = pos_07
-rom[8]  = pos_08
-rom[9]  = pos_09
-rom[10] = pos_10
-rom[11] = pos_11
-rom[12] = pos_12
-rom[13] = pos_13
-rom[14] = pos_14
-rom[15] = pos_15
-rom[16]  = pos_16
-rom[17]  = pos_17
-rom[18]  = pos_18
-rom[19]  = pos_19
-rom[20]  = pos_20
-rom[21]  = pos_21
-rom[22]  = pos_22
-rom[23]  = pos_23
-rom[24]  = pos_24
-rom[25]  = pos_25
-rom[26] = pos_26
-rom[27] = pos_27
-rom[28] = pos_28
-rom[29] = pos_29
-rom[30] = pos_30
-rom[31] = pos_31
-
+proglength = len(program)
+for i in range (0, proglength):
+    rom[i] = program[i]
+    print(hex(rom[i]))
+    
+#print("-5 :")
+#print(bin(-5 & 0xFF))
+#print(hex(-5 & 0xFF))
 
 with open("left_rom.bin", "wb") as out_file:
     out_file.write(rom);
